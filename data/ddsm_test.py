c15e6ba49fc567b5cd0497cab95382f9c6fa0f5b
xchen147 <36751725+xchen147@users.noreply.github.com>
2022-07-12 17:44:23 -0700

    cleanup

--- data/ddsm_test.py
+++ data/ddsm_test.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 """Tests for ddsm.py."""
 
-from garcon.pactran_metrics.data import data_testing_lib
+from pactran_metrics.data import data_testing_lib
 from pactran_metrics.data import ddsm
 import tensorflow.compat.v1 as tf
 
